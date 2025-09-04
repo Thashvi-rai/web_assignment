ce3a08ae6317bd4db73eb5135bb89d62e7f5d646
Thashvi Rai K <thashvirai1130.tr@gmail.com>
2025-09-04 12:44:46 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -7,10 +7,9 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     }, {
-        threshold: 0.1 // Animate when 10% of the element is visible
+        threshold: 0.1 
     });
 
-    // Find all elements to animate and start observing them
     const animatedElements = document.querySelectorAll('.hero-text, .hero-image, .passion-card');
     animatedElements.forEach(el => observer.observe(el));
 });
